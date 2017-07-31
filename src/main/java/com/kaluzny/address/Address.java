f9bb6e3c1e6bd5b045360ce7d396d8c0ab03572f
Oleg <cryoengineer@gmail.com>
2017-07-31 16:55:03 +0300

    Added Address.java.

---
+++ src/main/java/com/kaluzny/address/Address.java
@@ -0,0 +1,56 @@
+package com.kaluzny.address;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.kaluzny.author.Author;
+import com.kaluzny.domain.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ADDRESSES")
+public class Address extends BaseEntity {
+
+    private String country;
+    private String city;
+    private Author author;
+
+    @Column(name = "AUTHOR_COUNTRY")
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Column(name = "AUTHOR_CITY")
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "address")
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", author=" + author +
+                '}';
+    }
+}
