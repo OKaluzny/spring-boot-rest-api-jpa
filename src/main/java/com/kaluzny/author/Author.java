1ac1afac05af46bccc25a81c860bd7ea2640aa6c
Oleg <cryoengineer@gmail.com>
2017-08-01 18:37:08 +0300

    Added @ManyToMany Author.

--- src/main/java/com/kaluzny/author/Author.java
+++ src/main/java/com/kaluzny/author/Author.java
@@ -4,19 +4,23 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.kaluzny.address.Address;
 import com.kaluzny.book.Book;
 import com.kaluzny.domain.BaseEntity;
+import com.kaluzny.publisher.Publisher;
 import org.hibernate.annotations.Formula;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "AUTHORS")
+@Access(AccessType.PROPERTY)
 public class Author extends BaseEntity {
 
     private int bookCount;
     private String name;
     private List<Book> books; //Books that author has written
     private Address address;
+    private Set<Publisher> publisher;
 
     @Formula("(" +
             "SELECT count(books.id) " +
@@ -48,6 +52,7 @@ public class Author extends BaseEntity {
     public void setBooks(List<Book> books) {
         this.books = books;
     }
+
     @OneToOne
     @JoinColumn(name = "address_fk")
     public Address getAddress() {
@@ -56,6 +61,18 @@ public class Author extends BaseEntity {
 
     public void setAddress(Address address) {
         this.address = address;
+    }
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "Author_publisher",
+            joinColumns = @JoinColumn(name = "author_fk"), inverseJoinColumns = @JoinColumn(name = "publisher_fk"))
+    public Set<Publisher> getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Set<Publisher> publisher) {
+        this.publisher = publisher;
     }
 
     @Override
